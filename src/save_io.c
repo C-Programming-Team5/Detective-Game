b5d2244d993ed1d6f857f94f3f9ca78552ba6249
osm1892 <osm1892@naver.com>
2018-11-22 00:22:19 +0900

    암호화 추가, 세이브 함수 일부 추가

--- src/save_io.c
+++ src/save_io.c
@@ -1,2 +1,30 @@
 ﻿#include "save_io.h"
+#include "main.h"
 
+int GetClearedQuizCount(Player * player, int id)
+{
+	int i = 0, result = 0;
+
+	for (i = 0; i < QUIZCOUNT; i++)
+	{
+		result += (player->cleared & (1 << i)) && 1;
+	}
+	return result;
+}
+
+void PrintSaveList(Player * player)
+{
+	int i = 0;
+
+	CLS;
+	for (i = 0; i < SAVESIZE; i++)
+	{
+		printf("%d번째 세이브\n", i);
+		printf("맞힌 퀴즈 개수: %d\n", GetClearedQuizCount(player, i));
+	}
+}
+
+int InternelSave(Player * player, int id)
+{
+	return 0;
+}
--- src/save_io.h
+++ src/save_io.h
@@ -2,10 +2,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/*
+	Github의 ilvn / SHA256 repo에서 가져온 코드입니다.
+	SHA-256 알고리즘에 대한 자세한 설명은 https://csrc.nist.gov/csrc/media/publications/fips/180/4/final/documents/fips180-4-draft-aug2014.pdf
+	데이터를 암호화하는 SHA-256 해시 함수를 제공합니다. 세이브의 무결성 검증을 위해 사용합니다.
+*/
+#include "sha256.h"
 
-typedef struct player //player.h로 이동할 수도 있습니다.
+#define SAVESIZE 5 // 최대 세이브 개수를 지정하는 상수입니다.
+
+typedef struct player // 세이브를 위한 플레이어 구조체로, 후에 player.h로 이동할 수도 있습니다.
 {
-    int id; // 쯔꾸르에서 흔히 쓰이는 여러개의 세이브를 위한 식별 코드입니다. 0에서 4 사이의 숫자가 들어갑니다.
-    unsigned cleared; // 2진수 마스킹 형식으로 저장됩니다.
-    unsigned playTime;
+    unsigned cleared; // 무엇을 클리어했는지 2진수 마스킹 형태로 저장하는 멤버입니다.
+    unsigned playTime; // 플레이 시간을 저장하는 멤버입니다.
 } Player;
+
+// 클리어한 퀴즈의 개수를 반환하는 함수입니다.
+int GetClearedQuizCount(Player *player, int id);
+// 화면을 비우고, 세이브 목록을 나타내는 함수입니다.
+void PrintSaveList(Player *player);
+int InternelSave(Player *player, int id);
