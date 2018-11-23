053ac8218c683e92faddc2c6edb956e70805d60a
osm1892 <osm1892@naver.com>
2018-11-24 01:23:21 +0900

    enum 상수 정의, 스톱워치 함수

---
+++ src/player.c
@@ -0,0 +1,18 @@
+﻿#include "player.h"
+
+clock_t StopWatch(int flag)
+{
+    static clock_t start = 0, end = 0;
+    
+    switch (flag)
+    {
+        case START:
+            start = clock();
+            return 0;
+        case END:
+            end = clock();
+            return (end - start) / CLOCKS_PER_SEC;
+        default:
+            return 0;
+    }
+}
