fa514d2e5917089c154a808dba243492d09e5e26
osm1892 <osm1892@naver.com>
2018-11-27 12:49:10 +0900

    Screen 관련 함수를 screen.c로 이동

--- src/screen.c
+++ src/screen.c
@@ -92,3 +92,261 @@ void PrintSaveList(const Player save[])
 	}
 	return;
 }
+
+void Prologue(void)
+{
+	gotoxy(1, 25); printf(".");
+	Sleep(1000);
+	gotoxy(2, 25); printf(".");
+	Sleep(1000);
+	gotoxy(3, 25); printf(".");
+	Sleep(1000);
+	gotoxy(4, 25); printf(".");
+	Sleep(1000);
+	gotoxy(5, 25); printf(".");
+	Sleep(1000);
+	gotoxy(6, 25); printf(".");
+	Sleep(2000);
+	gotoxy(1, 25); printf("지끈거리는 머리를 붙잡으며 정신을 차렸다.");
+	Sleep(5000);
+	gotoxy(1, 25); printf("                                                                                       ");
+	Sleep(100);
+	gotoxy(1, 25); printf("어제 나는 분명 집에 간 것 같았는데, 강의실 안이다.");
+	Sleep(5000);
+	gotoxy(1, 25); printf("                                                                                       ");
+	Sleep(100);
+	gotoxy(1, 25); printf("집에 가기 위해 강의실문을 열었다.");
+	Sleep(5000);
+	gotoxy(1, 25); printf("                                                                                       ");
+	Sleep(100);
+	gotoxy(1, 25); printf("자물쇠가 내부에 설치 되어있어 열리지 않는다.");
+	Sleep(5000);
+	gotoxy(1, 25); printf("                                                                                       ");
+	Sleep(100);
+	gotoxy(1, 25); printf("휴대폰도 켜지지 않는다. 강의실을 나가기 위해선 5개의 비밀번호가 필요한 것 같다.");
+	Sleep(5000);
+	gotoxy(1, 25); printf("                                                                                       ");
+	Sleep(100);
+	gotoxy(1, 25); printf("혹시라도 조교가 두고 간 비밀번호 종이가 있을 수도 있기 때문에 강의실 내부를 찾아보기로 했다.");
+	Sleep(5000);
+	CLS; //프롤로그 대사창을 모두 지웁니다.
+}
+
+void LobbyScreen(void) //아스키 코드로 구현한 그림입니다.
+{
+	printf("\n");
+	printf("                    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n");
+	printf("                   ┃ ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ ┃\n");
+	printf("                   ┃ ┃                                                          ┃ ┃\n");
+	printf("   ┌─────┐         ┃ ┃                                                          ┃ ┃\n");
+	printf("   │공 지│         ┃ ┃                                                          ┃ ┃\n");
+	printf("   │     │         ┃ ┃                                                          ┃ ┃\n");
+	printf("   └─────┘         ┃ ┃                                                          ┃ ┃\n");
+	printf("                   ┃ ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ ┃\n");
+	printf("                    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n");
+	printf("                  ===================================================================\n");
+	printf("       ┏━━━━━━━━━━━━━━━┓         ┏━━━━━━━━━━━━━━━┓                  ┏━━━━━━━━━━━━━━━┓         ┏━━━━━━━━━━━━━━━┓       \n");
+	printf("       ┃ ■■■■■■■┃         ┃ ■■■■■■■┃                  ┃ ■■■■■■■┃         ┃ ■■■■■■■┃\n");
+	printf("       ┃ ■■■■■■■┃         ┃ ■■■■■■■┃                  ┃ ■■■■■■■┃         ┃ ■■■■■■■┃\n");
+	printf("       ┃ ■■■■■■■┃         ┃ ■■■■■■■┃                  ┃ ■■■■■■■┃         ┃ ■■■■■■■┃\n");
+	printf("      ─┃ ■■■■■■■┃─────────┃ ■■■■■■■┃─               ──┃ ■■■■■■■┃─────────┃ ■■■■■■■┃─\n");
+	printf("     / ┗━━━━━━━━━━━━━━━┛         ┗━━━━━━━━━━━━━━━┛  ＼           /  ┗━━━━━━━━━━━━━━━┛         ┗━━━━━━━━━━━━━━━┛  ＼\n");
+	printf("    /   ━━━━━┛   ┗━━━━━           ━━━━━┛   ┗━━━━━     ＼        /    ━━━━━┛   ┗━━━━━           ━━━━━┛   ┗━━━━━     ＼\n");
+	printf("   /   ┗━━━━━━━━━━━━━━━┛         ┗━━━━━━━━━━━━━━━┛      ＼     /    ┗━━━━━━━━━━━━━━━┛         ┗━━━━━━━━━━━━━━━┛      ＼\n");
+	printf("  ┌──────────────────────────────────────────────────────┐    ┌───────────────────────────────────────────────────────┐\n");
+	printf("  └──────────────────────────────────────────────────────┘    └───────────────────────────────────────────────────────┘\n");
+	printf("  │   │                                              │   │    │   │                                               │   │\n");
+	printf("  │   │                                              │   │    │   │                                               │   │\n");
+	printf("________________________________________________________________________________________________________________________\n");
+	//아래는 대사를 입력할 공간입니다.
+}
+
+int LobbyPlay(int choice)
+{
+	gotoxy(2, 25);  printf("이제 무엇을 할까?");
+	int POS = 5;
+	CursorView(0);
+	system("COLOR 0F");
+	while (!GetAsyncKeyState(VK_RETURN))
+	{
+		if (GetAsyncKeyState(VK_LEFT))
+		{
+			if (POS <= 0)
+			{
+				POS = 5;
+			}
+			else
+			{
+				POS -= 1;
+			}
+		}
+		else if (GetAsyncKeyState(VK_RIGHT))
+		{
+			if (POS >= 5)
+			{
+				POS = 0;
+			}
+			else
+			{
+				POS++;
+			}
+		}
+		switch (POS)
+		{
+		case 0:
+			SetColor(3);
+			gotoxy(1, 27); printf("1.물건을 찾아본다");
+			SetColor(15);
+			gotoxy(21, 27); printf("2.단서를 본다");
+			gotoxy(41, 27); printf("3.자물쇠를 연다");
+			gotoxy(61, 27); printf("4.저장한다");
+			gotoxy(81, 27); printf("5.종료한다");
+			break;
+		case 1:
+			SetColor(3);
+			gotoxy(1, 27); printf("1.물건을 찾아본다");
+			SetColor(15);
+			gotoxy(21, 27); printf("2.단서를 본다");
+			gotoxy(41, 27); printf("3.자물쇠를 연다");
+			gotoxy(61, 27); printf("4.저장한다");
+			gotoxy(81, 27); printf("5.종료한다");
+			break;
+		case 2:
+			gotoxy(1, 27); printf("1.물건을 찾아본다");
+			SetColor(3);
+			gotoxy(21, 27); printf("2.단서를 본다");
+			SetColor(15);
+			gotoxy(41, 27); printf("3.자물쇠를 연다");
+			gotoxy(61, 27); printf("4.저장한다");
+			gotoxy(81, 27); printf("5.종료한다");
+			break;
+		case 3:
+			gotoxy(1, 27); printf("1.물건을 찾아본다");
+			gotoxy(21, 27); printf("2.단서를 본다");
+			SetColor(3);
+			gotoxy(41, 27); printf("3.자물쇠를 연다");
+			SetColor(15);
+			gotoxy(61, 27); printf("4.저장한다");
+			gotoxy(81, 27); printf("5.종료한다");
+			break;
+		case 4:
+			gotoxy(1, 27); printf("1.물건을 찾아본다");
+			gotoxy(21, 27); printf("2.단서를 본다");
+			gotoxy(41, 27); printf("3.자물쇠를 연다");
+			SetColor(3);
+			gotoxy(61, 27); printf("4.저장한다");
+			SetColor(15);
+			gotoxy(81, 27); printf("5.종료한다");
+			break;
+		case 5:
+			gotoxy(1, 27); printf("1.물건을 찾아본다");
+			gotoxy(21, 27); printf("2.단서를 본다");
+			gotoxy(41, 27); printf("3.자물쇠를 연다");
+			gotoxy(61, 27); printf("4.저장한다");
+			SetColor(3);
+			gotoxy(81, 27); printf("5.종료한다");
+			SetColor(15);
+			break;
+		default:
+			break;
+		}
+		Sleep(100);
+	}
+	CLS;
+	return POS;
+}
+
+int SelectItem(int item)
+{
+	gotoxy(2, 25); printf("어떤 물건부터 찾아볼까?");
+	int POS = 5;
+	CursorView(0);
+	system("COLOR 0F");
+	while (!GetAsyncKeyState(VK_RETURN))
+	{
+		if (GetAsyncKeyState(VK_LEFT))
+		{
+			if (POS <= 0)
+			{
+				POS = 5;
+			}
+			else
+			{
+				POS -= 1;
+			}
+		}
+		else if (GetAsyncKeyState(VK_RIGHT))
+		{
+			if (POS >= 5)
+			{
+				POS = 0;
+			}
+			else
+			{
+				POS++;
+			}
+		}
+		switch (POS)
+		{
+		case 0:
+			SetColor(3);
+			gotoxy(1, 27); printf("1.컴퓨터");
+			SetColor(15);
+			gotoxy(21, 27); printf("2.책상 밑");
+			gotoxy(41, 27); printf("3.책상 위");
+			gotoxy(61, 27); printf("4.칠판");
+			gotoxy(81, 27); printf("5.포스터");
+			break;
+		case 1:
+			SetColor(3);
+			gotoxy(1, 27); printf("1.컴퓨터");
+			SetColor(15);
+			gotoxy(21, 27); printf("2.책상 밑");
+			gotoxy(41, 27); printf("3.책상 위");
+			gotoxy(61, 27); printf("4.칠판");
+			gotoxy(81, 27); printf("5.포스터");
+			break;
+		case 2:
+			gotoxy(1, 27); printf("1.컴퓨터");
+			SetColor(3);
+			gotoxy(21, 27); printf("2.책상 밑");
+			SetColor(15);
+			gotoxy(41, 27); printf("3.책상 위");
+			gotoxy(61, 27); printf("4.칠판");
+			gotoxy(81, 27); printf("5.포스터");
+			break;
+		case 3:
+			gotoxy(1, 27); printf("1.컴퓨터");
+			gotoxy(21, 27); printf("2.책상 밑");
+			SetColor(3);
+			gotoxy(41, 27); printf("3.책상 위");
+			SetColor(15);
+			gotoxy(61, 27); printf("4.칠판");
+			gotoxy(81, 27); printf("5.포스터");
+			break;
+		case 4:
+			gotoxy(1, 27); printf("1.컴퓨터");
+			gotoxy(21, 27); printf("2.책상 밑");
+			gotoxy(41, 27); printf("3.책상 위");
+			SetColor(3);
+			gotoxy(61, 27); printf("4.칠판");
+			SetColor(15);
+			gotoxy(81, 27); printf("5.포스터");
+			break;
+		case 5:
+			gotoxy(1, 27); printf("1.컴퓨터");
+			gotoxy(21, 27); printf("2.책상 밑");
+			gotoxy(41, 27); printf("3.책상 위");
+			gotoxy(61, 27); printf("4.칠판");
+			SetColor(3);
+			gotoxy(81, 27); printf("5.포스터");
+			SetColor(15);
+			break;
+		default:
+			break;
+		}
+		Sleep(100);
+	}
+	CLS;
+	return POS;
+}
