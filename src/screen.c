ec2e70ba56b297a86c425112d734d05366176dcd
osm1892 <osm1892@naver.com>
2018-12-22 23:18:59 +0900

    changed texts to Global constant, renamed GotoXY func

--- src/screen.c
+++ src/screen.c
@@ -37,7 +37,7 @@ void CursorView(char show)
 	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
 }
 
-void gotoxy(int x, int y)
+void GotoXY(int x, int y)
 {
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
@@ -48,9 +48,9 @@ void InitialPrint(void)
 	CLS;
 	system("title ESCAPE");
 
-	gotoxy(53, 15); fputs("새로 하기", stdout);
-	gotoxy(53, 17); fputs("이어 하기", stdout);
-	gotoxy(53, 19); fputs("게임 종료", stdout);
+	GotoXY(53, 15); fputs("새로 하기", stdout);
+	GotoXY(53, 17); fputs("이어 하기", stdout);
+	GotoXY(53, 19); fputs("게임 종료", stdout);
 }
 
 int StartScreen(void)
@@ -72,11 +72,11 @@ int StartScreen(void)
 		}
 
 		SetColor(POS == 0 ? 3 : 15);
-		gotoxy(53, 15); fputs("새로 하기", stdout);
+		GotoXY(53, 15); fputs("새로 하기", stdout);
 		SetColor(POS == 1 ? 3 : 15);
-		gotoxy(53, 17); fputs("이어 하기", stdout);
+		GotoXY(53, 17); fputs("이어 하기", stdout);
 		SetColor(POS == 2 ? 3 : 15);
-		gotoxy(53, 19); fputs("게임 종료", stdout);
+		GotoXY(53, 19); fputs("게임 종료", stdout);
 		SetColor(15);
 		
 		Sleep(500);
@@ -87,14 +87,6 @@ int StartScreen(void)
 
 void PrintClues(const Player * const player, int number)
 {
-	char *clues[] = 
-	{
-		"단서1: 그것은 세상 어디에도 묶여있지 않다.\n",
-		"단서2 : 세상의 그 무엇보다 강하기도 하지만, 그 무엇보다 약하기도 하다.\n",
-		"단서3 : 눈에 보이진 않지만, 정말 많은 이름을 갖고 있다.\n",
-		"단서4 : 그것에게 멈춤이란 곧 죽음이다.\n",
-		"단서5: 그 무엇보다 가볍고, 그 무엇보다 자유로운 것\n",
-	};
 	CLS;
 
 	if (number == -1)
@@ -132,42 +124,52 @@ void PrintSaveList(const Player save[])
 
 void Prologue(void)
 {
+	int i = 0;
+
 	LobbyScreen();
-	gotoxy(1, 25); printf(".");
+	GotoXY(1, 25); printf(".");
 	Sleep(1000); 
-	gotoxy(2, 25); printf(".");
+	GotoXY(2, 25); printf(".");
 	Sleep(1000);
-	gotoxy(3, 25); printf(".");
+	GotoXY(3, 25); printf(".");
 	Sleep(1000);
-	gotoxy(4, 25); printf(".");
+	GotoXY(4, 25); printf(".");
 	Sleep(1000);
-	gotoxy(5, 25); printf(".");
+	GotoXY(5, 25); printf(".");
 	Sleep(1000);
-	gotoxy(6, 25); printf(".");
+	GotoXY(6, 25); printf(".");
+
+	GotoXY(1, 25);
+	for (i = 0; i < 6; i++)
+	{
+		printf(".");
+		Sleep(1000);
+	}
+
 	Sleep(1500);
 	PlaySound(TEXT("searching.wav"), NULL, SND_ASYNC);//소리를 재생하고 바로 다음코드를 실행합니다.
-	gotoxy(1, 25); printf("지끈거리는 머리를 붙잡으며 정신을 차렸다.");
+	GotoXY(1, 25); printf("지끈거리는 머리를 붙잡으며 정신을 차렸다.");
 	Sleep(3000);
-	gotoxy(1, 25); printf("                                                                                       ");
+	GotoXY(1, 25); printf("                                                                                       ");
 	Sleep(100);
-	gotoxy(1, 25); printf("어제 나는 분명 집에 간 것 같았는데, 강의실 안이다.");
+	GotoXY(1, 25); printf("어제 나는 분명 집에 간 것 같았는데, 강의실 안이다.");
 	Sleep(3000);
-	gotoxy(1, 25); printf("                                                                                       ");
+	GotoXY(1, 25); printf("                                                                                       ");
 	Sleep(100);
-	gotoxy(1, 25); printf("집에 가기 위해 강의실문을 열었다.");
+	GotoXY(1, 25); printf("집에 가기 위해 강의실문을 열었다.");
 	Sleep(3000);
-	gotoxy(1, 25); printf("                                                                                       ");
+	GotoXY(1, 25); printf("                                                                                       ");
 	Sleep(100);
 	PlaySound(TEXT("lock.wav"), NULL, SND_ASYNC);
-	gotoxy(1, 25); printf("자물쇠가 내부에 설치 되어있어 열리지 않는다.");
+	GotoXY(1, 25); printf("자물쇠가 내부에 설치 되어있어 열리지 않는다.");
 	Sleep(3000);
-	gotoxy(1, 25); printf("                                                                                       ");
+	GotoXY(1, 25); printf("                                                                                       ");
 	Sleep(100);
-	gotoxy(1, 25); printf("휴대폰도 켜지지 않는다. 강의실을 나가기 위해선 5개의 비밀번호가 필요한 것 같다.");
+	GotoXY(1, 25); printf("휴대폰도 켜지지 않는다. 강의실을 나가기 위해선 5개의 비밀번호가 필요한 것 같다.");
 	Sleep(3000);
-	gotoxy(1, 25); printf("                                                                                       ");
+	GotoXY(1, 25); printf("                                                                                       ");
 	Sleep(100);
-	gotoxy(1, 25); printf("혹시라도 조교가 두고 간 비밀번호 종이가 있을 수도 있기 때문에 강의실 내부를 찾아보기로 했다.");
+	GotoXY(1, 25); printf("혹시라도 조교가 두고 간 비밀번호 종이가 있을 수도 있기 때문에 강의실 내부를 찾아보기로 했다.");
 	Sleep(3000);
 	CLS; //프롤로그 대사창을 모두 지웁니다.
 }
@@ -207,7 +209,7 @@ int LobbyPlay(void)
 	int select = 0;
 	CLS;
 	LobbyScreen();
-	gotoxy(2, 25);  printf("이제 무엇을 할까? <키보드로 조작하고 엔터키를 눌러 결정한다.>");
+	GotoXY(2, 25);  printf("이제 무엇을 할까? <키보드로 조작하고 엔터키를 눌러 결정한다.>");
 	int POS = 4;
 	CursorView(0);
 	SetColor(15);
@@ -223,15 +225,15 @@ int LobbyPlay(void)
 		}
 
 		SetColor(POS == 0 ? 3 : 15);
-		gotoxy(1, 27); printf("*물건을 찾아본다");
+		GotoXY(1, 27); printf("*물건을 찾아본다");
 		SetColor(POS == 1 ? 3 : 15);
-		gotoxy(21, 27); printf("*단서를 본다");
+		GotoXY(21, 27); printf("*단서를 본다");
 		SetColor(POS == 2 ? 3 : 15);
-		gotoxy(41, 27); printf("*자물쇠를 연다");
+		GotoXY(41, 27); printf("*자물쇠를 연다");
 		SetColor(POS == 3 ? 3 : 15);
-		gotoxy(61, 27); printf("*저장한다");
+		GotoXY(61, 27); printf("*저장한다");
 		SetColor(POS == 4 ? 3 : 15);
-		gotoxy(81, 27); printf("*종료한다");
+		GotoXY(81, 27); printf("*종료한다");
 		SetColor(15);
 
 		Sleep(500);
@@ -242,7 +244,7 @@ int LobbyPlay(void)
 
 int SelectItem(void)
 {
-	gotoxy(2, 25); printf("어떤 물건부터 찾아볼까?");
+	GotoXY(2, 25); printf("어떤 물건부터 찾아볼까?");
 	int POS = 5;
 
 	CursorView(0);
@@ -259,17 +261,17 @@ int SelectItem(void)
 		}
 
 		SetColor(POS == 0 ? 3 : 15);
-		gotoxy(1, 27); printf("*컴퓨터");
+		GotoXY(1, 27); printf("*컴퓨터");
 		SetColor(POS == 1 ? 3 : 15);
-		gotoxy(21, 27); printf("*책상 밑");
+		GotoXY(21, 27); printf("*책상 밑");
 		SetColor(POS == 2 ? 3 : 15);
-		gotoxy(41, 27); printf("*책상 위");
+		GotoXY(41, 27); printf("*책상 위");
 		SetColor(POS == 3 ? 3 : 15);
-		gotoxy(61, 27); printf("*칠판");
+		GotoXY(61, 27); printf("*칠판");
 		SetColor(POS == 4 ? 3 : 15);
-		gotoxy(81, 27); printf("*포스터");
+		GotoXY(81, 27); printf("*포스터");
 		SetColor(POS == 5 ? 3 : 15);
-		gotoxy(101, 27); printf("*돌아가기");
+		GotoXY(101, 27); printf("*돌아가기");
 		SetColor(15);
 
 		Sleep(500);
@@ -281,35 +283,6 @@ int SelectItem(void)
 
 void Quiz(int number)
 {
-	char *quiz[][4] =
-	{
-		{
-			"'n'을 누르면 다음 페이지로 넘어갑니다",
-			"Q. 신분을 밝혀라!\n\n내가 A에게 다음과 같이 물었다고 해보자.\n나:여러분 가운데 기사는 몇 분이나 되십니까 ? \n이에 A가 대답했으나 발음이 불분명하여 B가 추가적으로 대답했다.\nB:A는 우리중 기사는 한 명이라고 말했다.그 순간 C가 끼어들었다.\nC:B는 지금 거짓말을 하고있습니다.",
-			"B와 C의 신분을 밝혀라.(신분은 건달 혹은 기사이다)",
-		},
-		{
-			"'n'을 누르면 다음 페이지로 넘어갑니다",
-			"Q.물고기 주인의 국적은?\n\n색깔이 다른 집이 일렬로 5채 있다.그리고 각 집에는 서로 다른 국적을 가진 사람들이 살고 있다.\n다섯 사람은 어떤 음료를 마시고, 어느 꽃을 기르고, 어느 동물을 키우고 있다.\n어느 두 사람도 마시는 음료, 기르는 꽃, 키우는 동물은 일치하지 않는다.",
-			"영국인은 빨간 집에 살고, 스웨덴인은 개를 키우며, 덴마크인은 차를 마신다.\n초록집은 하얀 집의 왼쪽 집이며, 초록집에 사는 사람은 커피를 마신다.\n장미를 기르는 사람은 새를 키우고, 노란집 사람은 라일락을 기른다.\n한 가운데 사는 사람은 우유를 마시고, 노르웨이인은 첫번째 집에 산다.\n안개꽃을 기르는 사람은 고양이를 키우는 사람의 옆집에 산다.",
-			"프리지아를 기르는 사람은 맥주를 마신다.\n독일인은 사루비아를 기르며, 노르웨이인은 파란집 옆에 산다.\n안개꽃을 기르는 사람은 사람은 생수를 마시는 사람과 이웃이다.\n\n그렇다면 물고기를 키우는 사람은 어느 나라의 사람일까?",
-		},
-		{
-			"'n'을 누르면 다음 페이지로 넘어갑니다",
-			"Q.살아남자 미스터 화이트!\n\n세 명의 총잡이가 서로 동시에 결투를 벌인다.\n1.미스터 블랙은 명중률 100 % 의 사격 실력을 가지고 있다.\n2.미스터 그레이는 명중률 70 % 의 사격 실력을 갖고 있다.\n",
-			"3.미스터 화이트는 명중률 30 % 의 사격 실력을 가지고 있다.\n그리고 총잡이들은 서로의 실력차를 감안해서, 화이트->그레이->블랙 순서대로 발포하기로 하며, 한 번에 한 발만 쏠 수 있다.\n이 때 화이트는 어떻게 쏴야 가장 생존률이 높은가 ?",
-		},
-		{
-			"'n'을 누르면 다음 페이지로 넘어갑니다",
-			"Q.범인은 바로 너!\n\n한 마을에 보석 강도 살인 사건이 발생했다.\n용의자는 조상호, 이수억, 최수호, 박홍수로 총 4명이다.\n피해자는 다잉메시지로 '수어어' 라는 글자를 남겼다.",
-			"범인은 누구인가?",
-		},
-		{
-			"'n'을 누르면 다음 페이지로 넘어갑니다",
-			"Q.슈뢰딩거의 다이어리\n\n슈뢰딩거의 다이어리에는 요일마다 알 수 없는 숫자가 적혀 있다.\n물음표에 들어갈 숫자는 무엇인가 ?",
-			"MON = 3\nTUE = 5\nWED = 4\nTHU = ?",
-		},
-	};
 	int index[] = {3, 4, 3, 3, 3};
 	int i = 0;
 	vvfp quizScreen[5] = {Quiz1Screen, Quiz2Screen, Quiz3Screen, Quiz4Screen, Quiz5Screen};
@@ -318,7 +291,7 @@ void Quiz(int number)
 	for (i = 0; i < index[number]; i++)
 	{
 		quizScreen[number]();
-		gotoxy(1, 25);
+		GotoXY(1, 25);
 		puts(quiz[number][i]);
 		WAITFORKEY('n');
 		CLS;
@@ -328,39 +301,6 @@ void Quiz(int number)
 
 void Answer(Player *player, int number)
 {
-	char *answer[][5] = 
-	{
-		{
-			"*기사/기사",
-			"*기사/건달",
-			"*건달/기사",
-			"*건달/건달",
-		},
-		{
-			"*독일",
-			"*스웨덴",
-			"*영국",
-			"*덴마크",
-			"*노르웨이",
-		},
-		{
-			"1. 블랙",
-			"2. 그레이",
-			"3. 허공",
-		},
-		{
-			"*조상호",
-			"*이수억",
-			"*최수호",
-			"*박홍수",
-		},
-		{
-			"*1. 2",
-			"*2. 4",
-			"*3. 6",
-			"*4. 8",
-		},
-	};
 	int index[] = {4, 5, 3, 4, 4};
 	int rightAnswer[] = {2, 0, 2, 0, 2};
 	int POS = 0, i = 0;
@@ -385,7 +325,7 @@ void Answer(Player *player, int number)
 		for (i = 0; i < index[number]; i++)
 		{
 			SetColor(POS == i ? 3 : 15);
-			gotoxy(21 + i * 20, 27);
+			GotoXY(21 + i * 20, 27);
 			fputs(answer[number][i], stdout);
 		}
 		Sleep(500);
@@ -396,19 +336,19 @@ void Answer(Player *player, int number)
 
 	if (POS == rightAnswer[number])
 	{
-		gotoxy(1, 25);
+		GotoXY(1, 25);
 		printf("단서가 나타났다!");
 		player->cleared |= (1 << number);
-		gotoxy(1, 26);
+		GotoXY(1, 26);
 		PrintClues(player, number);
-		gotoxy(1, 27);
+		GotoXY(1, 27);
 		printf("'n'을 눌러 다른 물건도 찾아보자.");
 	}
 	else
 	{
-		gotoxy(1, 25);
+		GotoXY(1, 25);
 		printf("아무 일도 일어나지 않았다.");
-		gotoxy(1, 26);
+		GotoXY(1, 26);
 		printf("'n'을 눌러 다른 물건도 찾아보자.");
 	}
 	WAITFORKEY('n');
@@ -516,9 +456,9 @@ int OpenLock(void)
 
 	while (getchar() != '\n');
 	PrintDoor();
-	gotoxy(1, 25);
+	GotoXY(1, 25);
 	fputs("암호를 입력하세요: ____", stdout);
-	gotoxy(20, 25);
+	GotoXY(20, 25);
 	fgets(answer, sizeof(answer), stdin);
 
 	
@@ -687,13 +627,13 @@ void Quiz5Screen(void)
 
 void Title(void)
 {
-    gotoxy(36, 1); printf("\n");
-    gotoxy(36, 2); printf("\n");
-    gotoxy(36, 3); printf("     ___________  __________    ____   ______\n");
-    gotoxy(36, 4); printf("    / ____/ ___/ / ____/    |  / __ ＼/ ____/\n");
-    gotoxy(36, 5); printf("   / __/ ＼__ ＼/ /   / /|  | / /_/  / __/  \n");
-    gotoxy(36, 6); printf("  / /___ ___/ /  /___/ ___  |/ _____/ /___  \n");
-    gotoxy(36, 7); printf(" /_____//____/ ＼____/_/  |_|_/    /_____/\n");
+    GotoXY(36, 1); printf("\n");
+    GotoXY(36, 2); printf("\n");
+    GotoXY(36, 3); printf("     ___________  __________    ____   ______\n");
+    GotoXY(36, 4); printf("    / ____/ ___/ / ____/    |  / __ ＼/ ____/\n");
+    GotoXY(36, 5); printf("   / __/ ＼__ ＼/ /   / /|  | / /_/  / __/  \n");
+    GotoXY(36, 6); printf("  / /___ ___/ /  /___/ ___  |/ _____/ /___  \n");
+    GotoXY(36, 7); printf(" /_____//____/ ＼____/_/  |_|_/    /_____/\n");
 }
 
 void PrintEnding(void)
