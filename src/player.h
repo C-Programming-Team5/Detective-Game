a01c4b3d7554ce83a468fc6773d2e22b453680f6
osm1892 <osm1892@naver.com>
2020-08-19 02:34:34 +0900

    Update player.h
    주석을 업데이트 하였습니다.

--- src/player.h
+++ src/player.h
@@ -3,16 +3,20 @@
 
 #include <time.h>
 
+// 타이머 함수의 인자값을 위한 열거형입니다.
 enum
 {
 	START,
 	END
 };
 
-typedef struct player // 세이브를 위한 플레이어 구조체입니다.
+// 세이브를 위한 플레이어 구조체입니다.
+typedef struct player
 {
-	unsigned cleared; // 무엇을 클리어했는지 2진수 마스킹 형태로 저장하는 멤버입니다.
-	unsigned playTime; // 플레이 시간을 저장하는 멤버입니다.
+	// 무엇을 클리어했는지 2진수 마스킹 형태로 저장하는 멤버입니다.
+	unsigned cleared;
+	// 플레이 시간을 저장하는 멤버입니다.
+	unsigned playTime;
 } Player;
 
 // START를 받으면 시간을 초기화하고, END를 받으면 초기화 때부터 현재까지 지난 초를 반환합니다.
